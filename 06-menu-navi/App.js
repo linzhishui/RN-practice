ccaae2dd8b2f788efd76a28965637cca47f57dec
linzhishui <wangmiao27@gmail.com>
2024-01-06 00:08:29 +0800

    06 添加react navigation的引用

--- 06-menu-navi/App.js
+++ 06-menu-navi/App.js
@@ -1,13 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+import MealsNavigator from './navagation/MealsNavigator';
+
+const fetchFonts = () => {
+  Font.loadAsync({
+    'open-sans': require('./assets/fonts/Open-Sans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/Open-Sans-Bold.ttf'),
+  });
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  // if (!fontLoaded) {
+  //   return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)} />;
+  // }
+
+  return <MealsNavigator />;
 }
 
 const styles = StyleSheet.create({
